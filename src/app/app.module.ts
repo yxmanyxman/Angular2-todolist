b2fb3cfd4e90fc588db05f888658eb498a0d71f5
yxman <498571187@qq.com>
2017-04-12 19:20:13 +0800

    fix router redirection && view css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { TodoModule } from './todo/todo.module';
 import { routes } from './app.routes';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-import { TodoRoutes } from './todo/todo.routes';
-
+import { TodoRoutes } from './todo/todo.routes'; // 导入todoComponent
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: './todo/list',
+    redirectTo: '/todo/list',
     pathMatch: 'full'
   },
   ...TodoRoutes
